074e88529b2abc9ca1d3f6f3793f0080a0fb5d96
Valerochka1337 <valerochkayartsev88@gmail.com>
2022-12-07 00:49:55 +0300

    build_debug

---
+++ cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/valerijarcev/CLionProjects/UINT2022_T/cmake-build-debug/_deps/googletest-src
+# Build directory: /Users/valerijarcev/CLionProjects/UINT2022_T/cmake-build-debug/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/valerijarcev/CLionProjects/UINT2022_T/cmake-build-debug/_deps/googletest-src/googlemock
+# Build directory: /Users/valerijarcev/CLionProjects/UINT2022_T/cmake-build-debug/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
